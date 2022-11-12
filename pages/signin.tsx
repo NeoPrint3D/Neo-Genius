7450e02a460a64cf6420d0b0abbdb1641b2cedb2
NeoPrint3D <drew@ronsman.com>
2022-11-12 17:43:34 -0600

    added firebase hosting

---
+++ pages/signin.tsx
@@ -0,0 +1,9 @@
+export default function SignInPage() {
+    return (
+        <div className="">
+            <div className="">
+                Sign In
+            </div>
+        </div>
+    )
+}
---
+++ pages/signup.tsx
@@ -0,0 +1,9 @@
+export default function SignUpPage() {
+    return (
+        <div className="">
+            <div className="">
+                Sign Up
+            </div>
+        </div>
+    )
+}
